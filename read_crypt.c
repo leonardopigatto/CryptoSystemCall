f8f53b729213fe025d95c098e1c0b0c8dd2fdb81
Erik Bezerra Pereira da Silva <33065800+ErikPereira@users.noreply.github.com>
2019-11-28 11:16:03 -0300

    Finalização do projeto2
    Todas os requisitos finalizados.

--- read_crypt.c
+++ read_crypt.c
@@ -1,17 +1,21 @@
+#include<linux/sched.h>
+#include<linux/syscalls.h>
+#include <linux/file.h>
+#include <linux/fcntl.h>
+#include <linux/slab.h>
+#include <crypto/md5.h>
+#include <crypto/internal/skcipher.h>
+#include <crypto/rng.h>
 #include <linux/kernel.h>
-
 #include <linux/init.h>           
 #include <linux/module.h>         
 #include <linux/device.h>         
 #include <linux/kernel.h>         
 #include <linux/fs.h>            
-#include <linux/uaccess.h>          
-#include <linux/mutex.h>	 
+#include <linux/uaccess.h>          	 
 #include<linux/moduleparam.h>
 #include<linux/mm.h>
 #include <linux/scatterlist.h>  
-#include <linux/err.h>
-#include <linux/errno.h>
 #include <linux/kmod.h>
 #include <linux/string.h>
 #include <linux/completion.h>
@@ -20,16 +24,9 @@
 #include <crypto/hash.h>
 #include <linux/stat.h> 
 
-/* void hexdump(unsigned char *buf, unsigned int len);
- void crypt_SOB(char escolha);*/
+#define DATA_SIZE       16
 
-asmlinkage ssize_t write_crypt(int fd, const void *buf, size_t nbytes);
-{
-	printk("valores fd = %d\n buf = %s\n nbytes = %d\n", fd, *buf, nbytes);
-	return 0;
-}
-/*
- void hexdump(unsigned char *buf, unsigned int len)
+void hexdumpRead(unsigned char *buf, unsigned int len)
 {
 	while (len--){
 		printk("%2d %02x", len, *buf++);	
@@ -38,21 +35,101 @@ asmlinkage ssize_t write_crypt(int fd, const void *buf, size_t nbytes);
 	printk("\n");
 }
 
- void crypt_SOB(char message*, int len){
+int converteHexa(char valor){
+
+
+	switch(valor){
+
+		case '0':
+			return 00;
+		break;
+
+		case '1':
+			return 01;
+		break;
+
+		case '2':
+			return 02;
+		break;
+
+		case '3':
+			return 03;
+		break;
+
+		case '4':
+			return 04;
+		break;
+
+		case '5':
+			return 05;
+		break;
+
+		case '6':
+			return 06;
+		break;
+
+		case '7':
+			return 07;
+		break;
+
+		case '8':
+			return 8;
+		break;
+
+		case '9':
+			return 9;
+		break;
+
+		case 'a':
+		case 'A':
+			return 10;
+		break;
+
+		case 'b':
+		case 'B':
+			return 11;
+		break;
+
+		case 'c':
+		case 'C':
+			return 12;
+		break;
+
+		case 'd':
+		case 'D':
+			return 13;
+		break;
+
+		case 'e':
+		case 'E':
+			return 14;
+		break;
+
+		case 'f':
+		case 'F':
+			return 15;
+		break;
+	}
+}
+
+
+void decrypt_SOB(unsigned char message[], int tamanho){
 
 	
-	char *algo = "ecb(aes)"; // modificado: anterior "aes"
-	int   mode = CRYPTO_skcipher_MODE_CBC;
-	int   mask = CRYPTO_skcipher_MODE_MASK; // adicionado
-	int  i,j, k, zerar = 0;	
-	char  messageSaida[513] = "\0";
-	struct crypto_skcipher *tfm; // utilzamos skcipher ao inves de tfm
-	struct scatterlist sg[2]; // entender o q eh scatterlist
-	struct skcipher_request *req = NULL; // necessario apra criptografar
-	int    ret;
-	char   *input, *output;
-	char keyInterno[16] = "0123456789ABCDEF";
-		
+	
+	int   	mode = 0;
+	int   	mask = 0; 
+	int    	ret;
+
+	int  	i,j, k, zerar = 0;	
+	struct 	crypto_skcipher *tfm; 
+	struct 	scatterlist sg[2]; 
+	struct 	skcipher_request *req = NULL; 
+	
+		 char 	*algo = "ecb(aes)";
+	unsigned char 	*input, *output, *saida = NULL;
+		 char 	keyInterno[16] = "0123456789ABCDEF";
+	unsigned char  	messageSaida[513] = "\0";	
 
 
 	tfm = crypto_alloc_skcipher(algo, mode, mask);
@@ -62,28 +139,28 @@ asmlinkage ssize_t write_crypt(int fd, const void *buf, size_t nbytes);
 			return ;
 	}
 	
-	ret = crypto_skcipher_setkey(tfm, keyInterno, 16); // estava errado, o correto eh strlen, n sizeof
+	ret = crypto_skcipher_setkey(tfm, keyInterno, 16);
 
 	if (ret) {
-			printk(KERN_ERR PFX "erro no ret \n"); // FALTA FALAR SOBRE ESSE ERRO DE FLAG
+			printk("erro no ret \n"); 
 			goto out;
 	}
 
-	req = skcipher_request_alloc(tfm, GFP_KERNEL); // o que é GFP_KERNEL????
+	req = skcipher_request_alloc(tfm, GFP_KERNEL); 
 		if (!req) {
 		printk("could not allocate tfm request\n");
 		goto out;
 		}
 
-	input = kmalloc(DATA_SIZE, GFP_KERNEL); // Parametros estavam invertidos
+	input = kmalloc(DATA_SIZE, GFP_KERNEL); 
 	if (!input) {
-			printk(KERN_ERR PFX "kmalloc(input) failed\n");
+			printk("kmalloc(input) failed\n");
 			goto out;
 	}
 
-	output = kmalloc(DATA_SIZE, GFP_KERNEL); // Parametros estavam invertidos
+	output = kmalloc(DATA_SIZE, GFP_KERNEL); 
 	if (!output) {
-			printk(KERN_ERR PFX "kmalloc(output) failed\n");
+			printk("kmalloc(output) failed\n");
 			kfree(input);
 			goto out;
 	}
@@ -92,12 +169,12 @@ asmlinkage ssize_t write_crypt(int fd, const void *buf, size_t nbytes);
 	k = 0;
 	
 	
-	sg_init_one(&sg[0], input, DATA_SIZE);          //
-	sg_init_one(&sg[1], output, DATA_SIZE);      	//      entrou no lugar do FILL_SG();
+	sg_init_one(&sg[0], input, DATA_SIZE);          
+	sg_init_one(&sg[1], output, DATA_SIZE);      	
 
 	skcipher_request_set_crypt(req, &sg[0], &sg[1], DATA_SIZE, NULL); // ordem ods parametros: requisição, origem, destino, tamanho, iv;
 	
-	while( j < len)){
+	while( j < tamanho){
 
 		strcpy(input, "\0");
 
@@ -123,12 +200,12 @@ asmlinkage ssize_t write_crypt(int fd, const void *buf, size_t nbytes);
 		}
 		zerar = 0;
 		
-		crypto_skcipher_decrypt(req);	
+		crypto_skcipher_decrypt(req);
 
 		saida = sg_virt(&sg[1]);	
 		
-		printk("Entrada: "); hexdump(input, 16);
-		printk("Saida: "); hexdump(output, 16);
+		printk("Entrada: "); hexdumpRead(input, 16);
+		printk("Saida: "); hexdumpRead(output, 16);
 
 		for(i = 0; i < 16; i++){
 			messageSaida[k] = saida[i];
@@ -141,7 +218,7 @@ asmlinkage ssize_t write_crypt(int fd, const void *buf, size_t nbytes);
 	strcpy(message, messageSaida);
 	message[j] = '\0';
 
-	printk("Message: "); hexdump(message, 32);
+	printk("Message: "); hexdumpRead(message, tamanho);
 
 	kfree(output);
 	kfree(input);
@@ -149,7 +226,70 @@ asmlinkage ssize_t write_crypt(int fd, const void *buf, size_t nbytes);
 	out:
 	crypto_free_skcipher(tfm);
 	skcipher_request_free(req);
-	
-	return message;
 
-}*/
+}
+
+
+
+
+asmlinkage ssize_t read_crypt(int fd, const void *buf, size_t nbytes){
+
+	int i, hexa1, hexa2, len;
+	int tamanho = nbytes;
+	mm_segment_t old_fs;
+
+	unsigned char message[512];
+	
+	printk("entrei no read------------------------\n");
+
+	if(fd < 0){
+		return 1;
+	}
+	
+	old_fs = get_fs();
+	set_fs(KERNEL_DS);
+	sys_read(fd, buf, nbytes);
+	printk("leitura: %s", ((uint8_t*)buf));
+	printk("\n");
+
+	
+
+	hexa1 = converteHexa(((uint8_t*)buf)[0]);
+	hexa2 = converteHexa(((uint8_t*)buf)[1]);
+
+	hexa1 = hexa1 * 16;
+	hexa1 += hexa2;		
+	
+	for(i = 0; i < tamanho / 2; i++){
+
+		message[i] = hexa1;
+
+		hexa1 = converteHexa( ((uint8_t*)buf)[(i+1)*2] );
+		hexa2 = converteHexa( ((uint8_t*)buf)[((i+1) * 2) + 1] );
+
+		hexa1 = hexa1 * 16;
+		hexa1 += hexa2;
+
+	}
+	message[i] = '\0';
+	tamanho = strlen(message);
+
+	printk("valor message antes = "); hexdumpRead(message, tamanho);
+
+	decrypt_SOB(message, tamanho);
+
+	printk("valor message depois = "); hexdumpRead(message, tamanho);
+
+	for(i = 0; i < tamanho; i++){
+
+		((uint8_t*)buf)[i] = message[i];
+	
+	}
+	
+	printk("valor de retorno = %s\n", message);
+
+	set_fs(old_fs);
+	
+	
+	return 0;
+}
--- write_crypt.c
+++ write_crypt.c
@@ -1,35 +1,22 @@
-#include <linux/kernel.h>
-
-#include <linux/init.h>           
-#include <linux/module.h>         
-#include <linux/device.h>         
-#include <linux/kernel.h>         
-#include <linux/fs.h>            
-#include <linux/uaccess.h>          
-#include <linux/mutex.h>	 
-#include<linux/moduleparam.h>
-#include<linux/mm.h>
-#include <linux/scatterlist.h>  
+#include<linux/kernel.h>
+#include<linux/init.h>
+#include<linux/sched.h>
+#include<linux/syscalls.h>
+#include <linux/file.h>
+#include <linux/fs.h>
+#include <linux/fcntl.h>
+#include <asm/uaccess.h>
+#include <linux/slab.h>
 #include <linux/err.h>
-#include <linux/errno.h>
-#include <linux/kmod.h>
+#include <linux/scatterlist.h>
 #include <linux/string.h>
-#include <linux/completion.h>
-#include <crypto/skcipher.h>
-#include <linux/crypto.h>
-#include <crypto/hash.h>
-#include <linux/stat.h> 
+#include <crypto/md5.h>
+#include <crypto/internal/skcipher.h>
+#include <crypto/rng.h>
 
-/* void hexdump(unsigned char *buf, unsigned int len);
- void crypt_SOB(char escolha);*/
+#define DATA_SIZE       16
 
-asmlinkage ssize_t write_crypt(int fd, const void *buf, size_t nbytes);
-{
-	printk("valores fd = %d\n buf = %s\n nbytes = %d\n", fd, *buf, nbytes);
-	return 0;
-}
-/*
- void hexdump(unsigned char *buf, unsigned int len)
+void hexdump(unsigned char *buf, unsigned int len)
 {
 	while (len--){
 		printk("%2d %02x", len, *buf++);	
@@ -38,21 +25,25 @@ asmlinkage ssize_t write_crypt(int fd, const void *buf, size_t nbytes);
 	printk("\n");
 }
 
- void crypt_SOB(char message*, int len){
 
+
+void crypt_SOB(unsigned char message[], int tamanho){
+
+	printk("entrei\n");
 	
-	char *algo = "ecb(aes)"; // modificado: anterior "aes"
-	int   mode = CRYPTO_skcipher_MODE_CBC;
-	int   mask = CRYPTO_skcipher_MODE_MASK; // adicionado
-	int  i,j, k, zerar = 0;	
-	char  messageSaida[513] = "\0";
-	struct crypto_skcipher *tfm; // utilzamos skcipher ao inves de tfm
-	struct scatterlist sg[2]; // entender o q eh scatterlist
-	struct skcipher_request *req = NULL; // necessario apra criptografar
-	int    ret;
-	char   *input, *output;
-	char keyInterno[16] = "0123456789ABCDEF";
-		
+	int   	mode = 0;
+	int   	mask = 0; 
+	int    	ret;
+
+	int  	i,j, k, zerar = 0;	
+	struct 	crypto_skcipher *tfm; 
+	struct 	scatterlist sg[2]; 
+	struct 	skcipher_request *req = NULL; 
+	
+		 char 	*algo = "ecb(aes)";
+	unsigned char 	*input, *output, *saida = NULL;
+		 char 	keyInterno[16] = "0123456789ABCDEF";
+	unsigned char  	messageSaida[513] = "\0";	
 
 
 	tfm = crypto_alloc_skcipher(algo, mode, mask);
@@ -62,28 +53,28 @@ asmlinkage ssize_t write_crypt(int fd, const void *buf, size_t nbytes);
 			return ;
 	}
 	
-	ret = crypto_skcipher_setkey(tfm, keyInterno, 16); // estava errado, o correto eh strlen, n sizeof
+	ret = crypto_skcipher_setkey(tfm, keyInterno, 16);
 
 	if (ret) {
-			printk(KERN_ERR PFX "erro no ret \n"); // FALTA FALAR SOBRE ESSE ERRO DE FLAG
+			printk("erro no ret \n"); 
 			goto out;
 	}
 
-	req = skcipher_request_alloc(tfm, GFP_KERNEL); // o que é GFP_KERNEL????
+	req = skcipher_request_alloc(tfm, GFP_KERNEL); 
 		if (!req) {
 		printk("could not allocate tfm request\n");
 		goto out;
 		}
 
-	input = kmalloc(DATA_SIZE, GFP_KERNEL); // Parametros estavam invertidos
+	input = kmalloc(DATA_SIZE, GFP_KERNEL); 
 	if (!input) {
-			printk(KERN_ERR PFX "kmalloc(input) failed\n");
+			printk("kmalloc(input) failed\n");
 			goto out;
 	}
 
-	output = kmalloc(DATA_SIZE, GFP_KERNEL); // Parametros estavam invertidos
+	output = kmalloc(DATA_SIZE, GFP_KERNEL); 
 	if (!output) {
-			printk(KERN_ERR PFX "kmalloc(output) failed\n");
+			printk("kmalloc(output) failed\n");
 			kfree(input);
 			goto out;
 	}
@@ -92,12 +83,12 @@ asmlinkage ssize_t write_crypt(int fd, const void *buf, size_t nbytes);
 	k = 0;
 	
 	
-	sg_init_one(&sg[0], input, DATA_SIZE);          //
-	sg_init_one(&sg[1], output, DATA_SIZE);      	//      entrou no lugar do FILL_SG();
+	sg_init_one(&sg[0], input, DATA_SIZE);          
+	sg_init_one(&sg[1], output, DATA_SIZE);      	
 
 	skcipher_request_set_crypt(req, &sg[0], &sg[1], DATA_SIZE, NULL); // ordem ods parametros: requisição, origem, destino, tamanho, iv;
 	
-	while( j < len)){
+	while( j < tamanho){
 
 		strcpy(input, "\0");
 
@@ -141,7 +132,7 @@ asmlinkage ssize_t write_crypt(int fd, const void *buf, size_t nbytes);
 	strcpy(message, messageSaida);
 	message[j] = '\0';
 
-	printk("Message: "); hexdump(message, 32);
+	printk("Message: "); hexdump(message, tamanho);
 
 	kfree(output);
 	kfree(input);
@@ -149,7 +140,38 @@ asmlinkage ssize_t write_crypt(int fd, const void *buf, size_t nbytes);
 	out:
 	crypto_free_skcipher(tfm);
 	skcipher_request_free(req);
-	
-	return message;
 
-}*/
+}
+
+
+
+
+asmlinkage ssize_t write_crypt(int fd, const void *buf, size_t nbytes){
+
+	int i;
+	int tamanho = nbytes;
+	unsigned char message[512], arquivo[1024];
+
+	for(i = 0; i < tamanho; i++) {
+		sprintf(&message[i], "%c",((char *)buf)[i]);
+	}
+	printk("valor message = %s", message);
+	crypt_SOB(message, tamanho);
+
+	for(i = 0; i < tamanho; i++) {
+		sprintf(&arquivo[i*2], "%02x", message[i]);
+	}
+
+	printk("valor em arquivo = %s\n", arquivo);
+	
+	if (fd >= 0){
+		mm_segment_t old_fs = get_fs();
+		set_fs(KERNEL_DS);
+		sys_write(fd, arquivo, tamanho*2);
+		set_fs(old_fs);
+		return 0;
+  	}
+
+
+	return 1;
+}
